8b3f36679223fbe8294986ca9b6f29658cd2ee7e
Alan <guardavidas1987@gmail.com>
2022-05-31 02:19:50 -0300

    primero

---
+++ src/views/AboutView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="about pa-6">
+    <h1>Sobre Lista de tareas</h1>
+   <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum, optio.</p>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+     Mollitia maxime id obcaecati deserunt quos laudantium nesciunt harum qui placeat consectetur laboriosam, 
+     accusantium dolorum sunt culpa! Veniam consequuntur suscipit eligendi, nulla quaerat dolores doloribus, 
+     voluptates quas mollitia nobis fugiat? 
+     Distinctio aliquid exercitationem a vero harum quam veniam facere, pariatur velit est.</p>
+  </div>
+</template>
